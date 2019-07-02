babd633ea02ed2241a1fcc845f0008da561a97b3
idream-xu <xuhu_it@163.com>
2019-07-03 00:08:50 +0800

    modify sort code

--- src/main/java/com/xuhu/ds/sort/BubbleSort.java
+++ src/main/java/com/xuhu/ds/sort/BubbleSort.java
@@ -10,22 +10,25 @@ public class BubbleSort {
 
         int compareCount = 0;
         int len = arr.length;
+
+
         for (int i = 0; i < len - 1; i++) {
             boolean isCompare = false;
-            for (int j = i + 1; j < len; j++) {
+            for (int j = 0; j < len - 1 - i; j++) {
                 compareCount++;
-                if (arr[i] > arr[j]) {
+                if (arr[j] > arr[j + 1]) {
                     isCompare = true;
-                    int temp = arr[i];
-                    arr[i] = arr[j];
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
                     arr[j] = temp;
                 }
             }
 
             if (!isCompare) {
-//                break;
+                break;
             }
         }
+
 
         System.out.println("比较次数:" + compareCount);
         System.out.println(Arrays.toString(arr));
